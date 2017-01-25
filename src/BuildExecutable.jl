f3e3ea01cc1a6cc76557d761711e806c49f2889f
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2017-01-25 16:19:41 +0100

    Revert "sysji == NULL for static compilation": Does not work to load sys.so
    This reverts commit dc54b950a1fe46391338758866b83bbf7df7499e.

--- src/BuildExecutable.jl
+++ src/BuildExecutable.jl
@@ -130,8 +130,7 @@ function build_executable(exename::String,
 
     info("script_file : $script_file")
 
-    emit_cmain(cfile, exename, targetdir != nothing,
-               cpu_target=cpu_target, static=static)
+    emit_cmain(cfile, exename, targetdir != nothing, cpu_target=cpu_target)
     info("Created cfile $cfile")
     if compile_sys
         emit_userimgjl(userimgjl, script_file)
@@ -302,7 +301,7 @@ function get_includes(additional_includes::Bool = false)
     ret
 end
 
-function emit_cmain(cfile, exename, relocation; cpu_target="native", static::Bool=false)
+function emit_cmain(cfile, exename, relocation; cpu_target="native")
     if relocation
         sysji = joinpath("lib"*exename)
     else
@@ -319,11 +318,6 @@ function emit_cmain(cfile, exename, relocation; cpu_target="native", static::Boo
 
     call_postinit = "postinit();\n"
     define_postinit = "#include \"$(joinpath(dirname(@__FILE__), "..", "..", "build", "postinit.c"))\""
-    select_sysji = "sysji_env == NULL ? sysji : sysji_env"
-    if static
-        select_sysji = "sysji_env"
-    end
-        
     f = open(cfile, "w")
     write( f, """
         #include <julia.h>
@@ -393,7 +387,7 @@ function emit_cmain(cfile, exename, relocation; cpu_target="native", static::Boo
 
             assert(atexit(&failed_warning) == 0);
 
-            my_init_with_image(NULL, $select_sysji);
+            my_init_with_image(NULL, sysji_env == NULL ? sysji : sysji_env);
             if (getenv("JL_INFO"))
                 fprintf(stderr, "my_init_finished\\n");
             // set Base.ARGS, not Core.ARGS
